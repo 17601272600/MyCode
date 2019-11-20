6d690d8f87c8f378a43afec0d15ededbd27380e4
17601272600 <570571457@qq.com>
2019-11-20 08:28:56 +0800

    login 配置

--- src/main/java/com/qiantao/controller/UserController.java
+++ src/main/java/com/qiantao/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.qiantao.controller;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,22 +35,44 @@ public class UserController extends BaseController {
 	public String newAccount(HttpServletRequest request) {
 		
 		try {
-			;
-			String url="http://OAUTH/oauth/account/regeist";
+		
+			String url="http://oauth/oauth/account/regeist";
 			Map<String,String> map=new HashMap<String,String>();
-			String loginId=request.getParameter("telephone");
-				String pwd=(String)	request.getAttribute("pwd");
 			map.put("loginId",request.getParameter("loginId"));
-			map.put("password",request.getParameter("pwd"));
+			map.put("password",request.getParameter("passcode"));
 			ResponseVo result=restTemplate.postForObject(url, map, ResponseVo.class);
 			if(result.getIsSuc().equals(0)) {
-				
 				return "注冊成功";
 			}
 		} catch (Exception e) {
+			
 			e.printStackTrace();
 			LOG.error(e.getMessage(),e);
 		}
 		return "注冊失敗";
+	}
+	@RequestMapping("/text")
+	public String text(HttpServletRequest request,String telephone) {
+		System.out.println(telephone);
+		System.out.println("telephone by mvn "+telephone);
+		System.out.println("telephone by attr"+request.getAttribute("telephone"));
+		System.out.println("telephone by param"+request.getParameter("telephone"));
+		System.out.println("Attributions:");
+		 Enumeration<String>  attnames=request.getAttributeNames();
+		while (attnames.hasMoreElements()) {
+			String string = (String) attnames.nextElement();
+			System.out.println("attr:"+request.getAttribute(string));
+		}
+		System.out.println("end");
+		System.out.println("Parameter:");
+		attnames=request.getParameterNames();
+		
+		while (attnames.hasMoreElements()) {
+			String string = (String) attnames.nextElement();
+			System.out.println("attr:"+request.getParameter(string));
+		}
+		System.out.println("end");
+		
+		return null;
 	}
 }
